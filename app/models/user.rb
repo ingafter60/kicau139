d724f21e20493980c41b6c786e8d7b3eecac1112
ing <inyoman_gurnitha@yahoo.com>
2020-01-04 11:37:21 +0700

    Finish toy app

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+	has_many :microposts
+	validates :name, length: { maximum: 20}, presence: true    # Replace FILL_IN with the right code.
+   validates :email, length: { maximum: 50}, presence: true    # Replace FILL_IN with the right code.
+end
